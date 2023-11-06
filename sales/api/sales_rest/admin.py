fa2ad31f18593c8aa0563dbb3b333a712e7b629b
Minting <miz111@ucsd.edu>
2023-11-06 12:22:39 -0800

    modified admin view

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,11 +1,49 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
+
 from .models import AutomobileVO, SalesPerson, Customer, SalesRecord
 
 
-admin.site.register(AutomobileVO)
-admin.site.register(SalesPerson)
-admin.site.register(Customer)
-admin.site.register(SalesRecord)
+# admin.site.register(AutomobileVO)
+# admin.site.register(SalesPerson)
+# admin.site.register(Customer)
+# admin.site.register(SalesRecord)
+
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "vin",
+        "color",
+        "year",
+        "model_name",
+        "is_sold",
+    )
+
+@admin.register(SalesPerson)
+class SalesPersonAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "employee_number",
+    )
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "address",
+        "phone_number",
+    )
+
+@admin.register(SalesRecord)
+class SalesRecord(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "sales_person",
+        "customer",
+        "price",
+        "automobile",
+    )
