830337a0ccbe92177937d71997c0e2c61d762466
Minting <miz111@ucsd.edu>
2023-11-05 18:27:14 -0800

    added error catch with employee duplicate id

--- ghi/app/src/sales/CustomerForm.js
+++ ghi/app/src/sales/CustomerForm.js
@@ -1,100 +1,130 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-
 class CustomerForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            name: '',
-            address: '',
-            phoneNumber: '',
-        };
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleAddressChange = this.handleAddressChange.bind(this);
-        this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      address: "",
+      phoneNumber: "",
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const data = { ...this.state };
+    data.phone_number = data.phoneNumber;
+    delete data.phoneNumber;
+
+    const customerUrl = "http://localhost:8090/api/customer/";
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(customerUrl, fetchConfig);
+    if (response.ok) {
+      const alert = document.getElementById("success-message");
+      alert.classList.remove("d-none");
+      const cleared = {
+        name: "",
+        address: "",
+        phoneNumber: "",
+      };
+      this.setState(cleared);
     }
+  }
+  handleNameChange(event) {
+    const value = event.target.value;
+    this.setState({ name: value });
+  }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        const data = { ...this.state };
-        data.phone_number = data.phoneNumber;
-        delete data.phoneNumber;
+  handleAddressChange(event) {
+    const value = event.target.value;
+    this.setState({ address: value });
+  }
 
-        const customerUrl = 'http://localhost:8090/api/customer/';
-        const fetchConfig = {
-            method: "post",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        const response = await fetch(customerUrl, fetchConfig)
-        if (response.ok) {
-            const alert = document.getElementById("success-message");
-            alert.classList.remove("d-none");
-            const cleared = {
-                name: '',
-                address: '',
-                phoneNumber: ''
-            };
-            this.setState(cleared);
-        }
-    }
-    handleNameChange(event) {
-        const value = event.target.value;
-        this.setState({ name: value })
-    }
+  handlePhoneNumberChange(event) {
+    const value = event.target.value;
+    this.setState({ phoneNumber: value });
+  }
 
-    handleAddressChange(event) {
-        const value = event.target.value;
-        this.setState({ address: value })
-    }
+  render() {
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="mt-5 float-right">
+            <Link to="/customer">
+              <button className="col btn btn-outline-dark btn-rounded">
+                {" "}
+                ↩ Return to Customer List
+              </button>
+            </Link>
+          </div>
+          <div className="shadow p-4 mt-4">
+            <h1>Add a Customer</h1>
+            <form onSubmit={this.handleSubmit} id="create-customer-form">
+              <div className="form-floating mb-3">
+                <input
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                  placeholder="Customer name"
+                  required
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  value={this.state.address}
+                  onChange={this.handleAddressChange}
+                  placeholder="Address"
+                  required
+                  type="text"
+                  name="address"
+                  id="address"
+                  className="form-control"
+                />
+                <label htmlFor="address">Address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  value={this.state.phoneNumber}
+                  onChange={this.handlePhoneNumberChange}
+                  placeholder="Phone Number"
+                  required
+                  type="tel"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  name="phone_number"
+                  id="phone_number"
+                  className="form-control"
+                />
+                <label htmlFor="phone_number">Phone Number XXX-XXX-XXXX</label>
+              </div>
 
-    handlePhoneNumberChange(event) {
-        const value = event.target.value;
-        this.setState({ phoneNumber: value })
-    }
-
-    render() {
-        return (
-            <div className="row">
-                <div className="offset-3 col-6">
-                    <div className="mt-5 float-right">
-                        <Link to="/customer">
-                            <button className="col btn btn-outline-dark btn-rounded"> ↩ Return to Customer List</button>
-                        </Link>
-                    </div>
-                    <div className="shadow p-4 mt-4">
-                        <h1>Add a Customer</h1>
-                        <form onSubmit={this.handleSubmit} id="create-customer-form">
-                            <div className="form-floating mb-3">
-                                <input value={this.state.name} onChange={this.handleNameChange} placeholder="Customer name" required type="text" name="name" id="name"
-                                    className="form-control" />
-                                <label htmlFor="name">Name</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input value={this.state.address} onChange={this.handleAddressChange} placeholder="Address" required type="text" name="address" id="address"
-                                    className="form-control" />
-                                <label htmlFor="address">Address</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange} placeholder="Phone Number" required type="text" name="phone_number" id="phone_number"
-                                    className="form-control" />
-                                <label htmlFor="phone_number">Phone Number</label>
-                            </div>
-
-                            <button className="btn btn-dark">Create</button>
-                        </form>
-                        <div className="mt-5 alert alert-success d-none mb-0" id="success-message">
-                            Customer created!
-                        </div>
-                    </div>
-                </div>
+              <button className="btn btn-dark">Create</button>
+            </form>
+            <div
+              className="mt-5 alert alert-success d-none mb-0"
+              id="success-message"
+            >
+              Customer created!
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CustomerForm;
--- ghi/app/src/sales/SalesPersonForm.js
+++ ghi/app/src/sales/SalesPersonForm.js
@@ -1,85 +1,120 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 class SalesPersonForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            name: '',
-            employeeNumber: '',
-        };
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleEmployeeNumberChange = this.handleEmployeeNumberChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    async handleSubmit(event) {
-        event.preventDefault();
-        const data = { ...this.state };
-        data.employee_number = data.employeeNumber;
-        delete data.employeeNumber;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      employeeNumber: "",
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmployeeNumberChange =
+      this.handleEmployeeNumberChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const data = { ...this.state };
+    data.employee_number = data.employeeNumber;
+    delete data.employeeNumber;
 
-        const salesPersonUrl = 'http://localhost:8090/api/salesperson/';
-        const fetchConfig = {
-            method: "post",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        const response = await fetch(salesPersonUrl, fetchConfig)
-        if (response.ok) {
-            const alert = document.getElementById("success-message");
-            alert.classList.remove("d-none");
-            const cleared = {
-                name: '',
-                employeeNumber: ''
-            };
-            this.setState(cleared);
-        }
+    const salesPersonUrl = "http://localhost:8090/api/salesperson/";
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(salesPersonUrl, fetchConfig);
+    if (response.ok) {
+      const alert = document.getElementById("success-message");
+      alert.classList.remove("d-none");
+      const error_alert = document.getElementById("error-message");
+      error_alert.classList.add("d-none");
+      const cleared = {
+        name: "",
+        employeeNumber: "",
+      };
+      this.setState(cleared);
+    } else {
+      const alert = document.getElementById("error-message");
+      alert.classList.remove("d-none");
     }
+  }
 
-    handleNameChange(event) {
-        const value = event.target.value;
-        this.setState({ name: value })
-    }
+  handleNameChange(event) {
+    const value = event.target.value;
+    this.setState({ name: value });
+  }
 
-    handleEmployeeNumberChange(event) {
-        const value = event.target.value;
-        this.setState({ employeeNumber: value })
-    }
+  handleEmployeeNumberChange(event) {
+    const value = event.target.value;
+    this.setState({ employeeNumber: value });
+  }
 
-    render() {
-        return (
-            <div className="row">
-                <div className="offset-3 col-6">
-                    <div className="mt-5 float-right">
-                        <Link to="/salesperson">
-                            <button className="col btn btn-outline-dark btn-rounded"> ↩ Return to Sales Person List</button>
-                        </Link>
-                    </div>
-                    <div className="shadow p-4 mt-4">
-                        <h1>Add a New Sales Person</h1>
-                        <form onSubmit={this.handleSubmit} id="create-hat-form">
-                            <div className="form-floating mb-3">
-                                <input value={this.state.name} onChange={this.handleNameChange} placeholder="Style" required type="text" name="name" id="name"
-                                    className="form-control" />
-                                <label htmlFor="name">Name</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input value={this.state.employeeNumber} onChange={this.handleEmployeeNumberChange} placeholder="Employee Number" required type="text" name="employee_number" id="employee_number"
-                                    className="form-control" />
-                                <label htmlFor="color">Employee Number</label>
-                            </div>
-                            <button className="btn btn-dark">Create</button>
-                        </form>
-                        <div className="mt-5 alert alert-success d-none mb-0" id="success-message">
-                            Sales Person Added!
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="mt-5 float-right">
+            <Link to="/salesperson">
+              <button className="col btn btn-outline-dark btn-rounded">
+                {" "}
+                ↩ Return to Sales Person List
+              </button>
+            </Link>
+          </div>
+          <div className="shadow p-4 mt-4">
+            <h1>Add a New Sales Person</h1>
+            <form onSubmit={this.handleSubmit} id="create-hat-form">
+              <div className="form-floating mb-3">
+                <input
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                  placeholder="Style"
+                  required
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-control"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  value={this.state.employeeNumber}
+                  onChange={this.handleEmployeeNumberChange}
+                  placeholder="Employee Number"
+                  required
+                  type="text"
+                  name="employee_number"
+                  id="employee_number"
+                  className="form-control"
+                />
+                <label htmlFor="color">Employee Number</label>
+              </div>
+              <button className="btn btn-dark">Create</button>
+            </form>
+            <div
+              className="mt-5 alert alert-success d-none mb-0"
+              id="success-message"
+            >
+              Sales Person Added!
             </div>
-        )
-    }
+            <div
+              className="mt-5 alert alert-warning d-none mb-0"
+              id="error-message"
+            >
+              Unable to add sales person. Please validate employee number is not
+              already in use.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SalesPersonForm;
